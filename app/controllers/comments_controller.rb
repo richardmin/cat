73da44dfbc3e19a38e7f536049b9f5092a3270f1
Grace Tsang <gracetsang1996@gmail.com>
2016-11-28 21:48:17 -0800

    Add b.net ID and start making comments look like comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.where('id > ?', params[:after_id].to_i).order('created_at DESC')
+    @comments = Comment.where('id > ?', params[:after_id].to_i).order('created_at ASC')
   end
 
   def new
     @comment = Comment.new
-    @comments = Comment.order('created_at DESC')
+    @comments = Comment.order('created_at ASC')
   end
 
   def create
