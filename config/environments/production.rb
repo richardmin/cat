7dcd799c7bda865d41f4bc1b4c49cc6aeb772b71
Grace Tsang <gracetsang1996@gmail.com>
2016-12-01 00:56:36 -0800

    Fix background for prod attempt 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.serve_static_assets = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_files = false
+  config.assets.compile = true
+  config.assets.digest = true
 end
